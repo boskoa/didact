b009043022320feafc36571e749ddf6a348f354d
boskoa <bosko.ap@gmail.com>
2023-01-19 23:26:35 +0100

    Adding render and commit phases

--- src/didact.js
+++ src/didact.js
@@ -30,20 +30,39 @@ function createDom(fiber) {
   Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => (dom[name] = fiber.props[name]));
-
+  console.log("CREATE DOM", dom);
   return dom;
 }
 
+function commitRoot() {
+  commitWork(wipRoot.child);
+  wipRoot = null;
+}
+
+function commitWork(fiber) {
+  if (!fiber) {
+    return;
+  }
+
+  const domParent = fiber.parent.dom;
+  domParent.appendChild(fiber.dom);
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
+}
+
 function render(element, container) {
-  nextUnitOfWork = {
+  wipRoot = {
     dom: container,
     props: {
       children: [element],
     },
   };
+
+  nextUnitOfWork = wipRoot;
 }
 
 let nextUnitOfWork = null;
+let wipRoot = null;
 
 function workLoop(deadline) {
   let shouldYield = false;
@@ -51,6 +70,11 @@ function workLoop(deadline) {
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
     shouldYield = deadline.timeRemaining() < 1;
   }
+
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot();
+  }
+
   requestIdleCallback(workLoop);
 }
 
@@ -58,14 +82,19 @@ requestIdleCallback(workLoop);
 
 function performUnitOfWork(fiber) {
   // add dom node
+  const entry = { ...fiber };
+  console.log("ENTRY", entry);
   if (!fiber.dom) {
     fiber.dom = createDom(fiber);
   }
+  /*
   if (fiber.parent) {
     fiber.parent.dom.appendChild(fiber.dom);
   }
+  */
   // create new fibers
   const elements = fiber.props.children;
+  console.log("ELEMENTS", elements);
   let index = 0;
   let prevSibling = null;
 
@@ -78,11 +107,13 @@ function performUnitOfWork(fiber) {
       parent: fiber,
       dom: null,
     };
-
+    console.log("FOOFOO", prevSibling);
     if (index === 0) {
       fiber.child = newFiber;
+      console.log("INDEX 0", fiber);
     } else {
       prevSibling.sibling = newFiber;
+      console.log("PREV SIBLING", { ...prevSibling }, fiber);
     }
 
     prevSibling = newFiber;
@@ -90,15 +121,19 @@ function performUnitOfWork(fiber) {
   }
   // return next unit of work
   if (fiber.child) {
+    const fibChil = { ...fiber.child };
+    console.log("HAS CHILD", fibChil);
     return fiber.child;
   }
   let nextFiber = fiber;
   while (nextFiber) {
     if (nextFiber.sibling) {
+      console.log("HAS SIBLING", nextFiber);
       return nextFiber.sibling;
     }
     nextFiber = nextFiber.parent;
   }
+  console.log("FINAL", fiber);
 }
 
 const Didact = {
